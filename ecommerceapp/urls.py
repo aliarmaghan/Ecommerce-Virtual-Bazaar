a9733889f91a3bff52432d8e0614754565190325
aliarmaghan <aliarmaghan78@gmail.com>
2023-09-04 23:54:56 +0530

    Checkout-Backend-Logic

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index , name ="index"),
     path('contact/', views.contact , name ="contact"),
-    path('about/', views.about , name ="about")
+    path('about/', views.about , name ="about"),
+    path('checkout/', views.checkout , name ="checkout"),
 ]
 
 
--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from ecommerceapp.models import Contact,Product
+from django.shortcuts import render,redirect
+from ecommerceapp.models import Contact,Product,OrderUpdate,Orders
 from django.contrib import messages
 from math import ceil
 
@@ -32,3 +32,39 @@ def contact(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def checkout(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Login and Try Again")
+        return redirect('/auth/login')
+    
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJson','')
+        name = request.POST.get('name','')
+        amount = request.POST.get('amt','')
+        email = request.POST.get('email','')
+        address1 = request.POST.get('address1','')
+        address2 = request.POST.get('address2','')
+        city = request.POST.get('city','')
+        state = request.POST.get('state','')
+        zip_code = request.POST.get('zip_code','')
+        phone = request.POST.get('phone','')
+        Order = Orders(items_json = items_json,name = name,amount=amount,email = email,address1=address1,address2=address2,city=city,
+                      state=state,zip_code = zip_code,phone=phone)
+        print(amount)
+        Order.save()
+        update = OrderUpdate(order_id = Order.order_id,update_desc ="The order has been placed")
+        update.save()
+        thank = True
+
+
+# Payment Integration
+
+    # id = Order.order_id
+
+
+
+
+    
+    #     return render(request, "checkout.html")
